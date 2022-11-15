307314b78e63d2a3184394e18d2783ed4484126e
Maia Ferrer <maiaferrer@Maias-MacBook-Air.local>
2022-11-14 20:38:50 -0500

    added past,present, and future

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
-// in the html. --- loads the page only and if jquery is able to be ran
+// in the html. --- loads the page only and if jquery is able to be ran -- done
 
 $(document).ready(function () {
-  var d = new Date();
+  // current day function
+  $("#currentDay").text(dayjs().format('dddd, MMMM D, YYYY'));
 
-  var month = d.getMonth() + 1;
-  var day = d.getDate();
+console.log(dayjs().format('H'));
 
-  var output =
-    d.getFullYear() +
-    "/" +
-    (month < 10 ? "0" : "") +
-    month +
-    "/" +
-    (day < 10 ? "0" : "") +
-    day;
+$(".time-block").each(function(){
+  var currentHour = parseInt(dayjs().format('H'));
+  var blockHour = parseInt($(this).attr("id").split("-")[1]);
+  console.log(blockHour);
 
-  $("#currentDay").text(output);
+  if(blockHour < currentHour)
+  {
+    $(this).addClass("past");
+  }
+  else if(blockHour === currentHour)
+  {
+     $(this).removeClass("past");
+     $(this).addClass("present");
+  }
+  else{
+    $(this).removeClass("past");
+    $(this).removeClass("present");
+    $(this).addClass("future");
+  }
+})
+
+
+  // saves responses to local storage 
   $("button").click(function () {
-    $(this).hide();
+    localStorage.getItem("");
   });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -38,5 +51,5 @@ $(document).ready(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  // TODO: Add code to display the current date in the header of the page. -- done
 });
